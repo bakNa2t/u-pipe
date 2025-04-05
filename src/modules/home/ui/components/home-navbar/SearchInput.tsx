730d92255ab5cbb770d96c4675bcdfccaff8441e
bakNa2t <ok2.lock@gmail.com>
2025-04-05 13:00:03 +0300

    Add SearchIcon submit btn

--- src/modules/home/ui/components/home-navbar/SearchInput.tsx
+++ src/modules/home/ui/components/home-navbar/SearchInput.tsx
@@ -1,15 +1,22 @@
+import { SearchIcon } from "lucide-react";
+
 export const SearchInput = () => {
   return (
-    <form>
-      <div className="flex w-full max-w-[600px]">
-        <div className="relative w-full">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="w-full pl-4 pr-12 py-2 rounded-l-full border focus:outline-none focus:border-violet-900"
-          />
-        </div>
+    <form className="flex w-full max-w-[600px]">
+      <div className="relative w-full">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-full pl-4 pr-12 py-2 rounded-l-full border focus:outline-none focus:border-violet-900"
+        />
       </div>
+
+      <button
+        type="submit"
+        className="px-5 py-2.5 rounded-r-full bg-violet-900/40 border-l-0 hover:bg-violet-900/60 disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        <SearchIcon className="size-5" />
+      </button>
     </form>
   );
 };
