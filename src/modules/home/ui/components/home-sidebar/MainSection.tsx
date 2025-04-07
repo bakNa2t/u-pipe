96717dbc54c55bfd7d0bc89672b47bc3ef331152
bakNa2t <ok2.lock@gmail.com>
2025-04-07 10:23:26 +0300

    Create and add sidebar's MainSection to home-sidebar

---
+++ src/modules/home/ui/components/home-sidebar/MainSection.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import Link from "next/link";
+import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const menuItems = [
+  {
+    title: "Home",
+    url: "/",
+    icon: HomeIcon,
+  },
+  {
+    title: "Subscriptions",
+    url: "/feed/subscriptions",
+    icon: PlaySquareIcon,
+    auth: true,
+  },
+  {
+    title: "Trending",
+    url: "/feed/trending",
+    icon: FlameIcon,
+  },
+];
+
+export const MainSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {menuItems.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                asChild
+                tooltip={item.title}
+                isActive={false}
+                onClick={() => {}}
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
--- src/modules/home/ui/components/home-sidebar/index.tsx
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -1,9 +1,13 @@
 import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
 
+import { MainSection } from "./MainSection";
+
 export const HomeSidebar = () => {
   return (
     <Sidebar className="pt-16 border-none z-40">
-      <SidebarContent className="bg-background"></SidebarContent>
+      <SidebarContent className="bg-background">
+        <MainSection />
+      </SidebarContent>
     </Sidebar>
   );
 };
