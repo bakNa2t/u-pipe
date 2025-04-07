5f717be073df9617ddedfa1bd6abad87c46885ae
bakNa2t <ok2.lock@gmail.com>
2025-04-07 11:14:26 +0300

    Create and add PersonalSection to home-sidebar

---
+++ src/modules/home/ui/components/home-sidebar/PersonalSection.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import Link from "next/link";
+import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const menuItems = [
+  {
+    title: "History",
+    url: "/playlsts/history",
+    icon: HistoryIcon,
+    auth: true,
+  },
+  {
+    title: "Liked videos",
+    url: "/playlists/liked",
+    icon: ThumbsUpIcon,
+    auth: true,
+  },
+  {
+    title: "All playlists",
+    url: "/playlists",
+    icon: ListVideoIcon,
+    auth: true,
+  },
+];
+
+export const PersonalSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>You</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {menuItems.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                asChild
+                tooltip={item.title}
+                isActive={false}
+                onClick={() => {}}
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
--- src/modules/home/ui/components/home-sidebar/index.tsx
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -1,12 +1,19 @@
 import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
 
+import { Separator } from "@/components/ui/separator";
+
 import { MainSection } from "./MainSection";
+import { PersonalSection } from "./PersonalSection";
 
 export const HomeSidebar = () => {
   return (
-    <Sidebar className="pt-16 border-none z-40">
+    <Sidebar className="pt-16 border-none z-40" collapsible="icon">
       <SidebarContent className="bg-background">
         <MainSection />
+
+        <Separator />
+
+        <PersonalSection />
       </SidebarContent>
     </Sidebar>
   );
