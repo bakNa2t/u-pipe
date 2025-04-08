7ec009e6d3a49c6a33f3d1fc1fca3f98134bd1d8
bakNa2t <ok2.lock@gmail.com>
2025-04-08 10:15:13 +0300

    Add clerk's SignedIn/Out comps

--- src/modules/auth/ui/components/AuthButton.tsx
+++ src/modules/auth/ui/components/AuthButton.tsx
@@ -1,15 +1,28 @@
+"use client";
+
 import { UserCircleIcon } from "lucide-react";
+import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
 
 export const AuthButton = () => {
   return (
-    <Button
-      variant="outline"
-      className="px-4 py-2 text-sm font-medium text-violet-500 hover:text-violet-700/80 border-violet-900/60 rounded-full shadow-none [&_svg]:size-6"
-    >
-      <UserCircleIcon />
-      Sign In
-    </Button>
+    <>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-violet-500 hover:text-violet-700/80 border-violet-900/60 rounded-full shadow-none [&_svg]:size-6"
+          >
+            <UserCircleIcon />
+            Sign In
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 };
