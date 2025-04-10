a12d1907d94504c7fa6f0be4712f407edc68d10d
bakNa2t <ok2.lock@gmail.com>
2025-04-10 15:28:18 +0300

    Add superjson pack

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -2,6 +2,7 @@
 // ^-- to make sure we can mount the Provider from a server component
 import type { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
+import superjson from "superjson";
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
@@ -39,7 +40,7 @@ export function TRPCProvider(
     trpc.createClient({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
           url: getUrl(),
         }),
       ],
