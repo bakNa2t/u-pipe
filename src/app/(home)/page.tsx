47260d4757c81c70b8afa38cc48aa636cdd7c79c
bakNa2t <ok2.lock@gmail.com>
2025-04-14 14:08:22 +0300

    Add trpc prefetch data and HydtateClient

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,30 @@
-import Image from "next/image";
+import { HydrateClient, trpc } from "@/trpc/server";
 
-export default function Home() {
+import { HomeView } from "@/modules/home/ui/views/HomeView";
+
+export const dynamic = "force-dynamic";
+
+interface PageProps {
+  searchParams: Promise<{
+    categoryId?: string;
+  }>;
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { categoryId } = await searchParams;
+
+  void trpc.categories.getMany.prefetch();
   return (
     <div>
-      <Image src="logo-u-pipe.svg" width={50} height={50} alt="logo" />
-      <p className="text-2xl font-semibold tracking-tight">Welcome to Upipe</p>
+      <h1 className="text-2xl font-semibold tracking-tight text-center">
+        Welcome to Upipe
+      </h1>
+
+      <HydrateClient>
+        <HomeView categoryId={categoryId} />
+      </HydrateClient>
     </div>
   );
-}
+};
+
+export default Page;
