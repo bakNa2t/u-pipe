ffe0005e5e0c7844d37a39026a21d235a9557b13
bakNa2t <ok2.lock@gmail.com>
2025-04-14 14:46:33 +0300

    Add carousel item, badge and previous/next arroes

--- src/components/filter-carousel.tsx
+++ src/components/filter-carousel.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Carousel } from "@/components/ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Badge } from "@/components/ui/badge";
 
 interface FilterCarouselProps {
   value?: string | null;
@@ -18,11 +25,43 @@ export const FilterCarousel = ({
   onSelect,
   data,
 }: FilterCarouselProps) => {
-  console.log(value, isLoading, onSelect, data);
+  console.log(onSelect);
 
   return (
     <div className="relative w-full">
-      <Carousel></Carousel>
+      <Carousel
+        opts={{
+          align: "start",
+          dragFree: true,
+        }}
+        className="w-full px-12"
+      >
+        <CarouselContent className="-ml-3">
+          <CarouselItem className="pl-3 basis-auto">
+            <Badge
+              variant={value === null ? "default" : "secondary"}
+              className="px-3 py-1 rounded-lg cursor-pointer whitespace-nowrap text-sm"
+            >
+              All
+            </Badge>
+          </CarouselItem>
+
+          {!isLoading &&
+            data.map((item) => (
+              <CarouselItem key={item.value} className="pl-3 basis-auto">
+                <Badge
+                  variant={value === item.value ? "default" : "secondary"}
+                  className="px-3 py-1 rounded-lg cursor-pointer whitespace-nowrap text-sm"
+                >
+                  {item.label}
+                </Badge>
+              </CarouselItem>
+            ))}
+        </CarouselContent>
+
+        <CarouselPrevious className="left-0 z-20" />
+        <CarouselNext className="right-0 z-20" />
+      </Carousel>
     </div>
   );
 };
