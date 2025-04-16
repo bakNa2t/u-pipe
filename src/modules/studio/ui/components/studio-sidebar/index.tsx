7787f924ff65fc0c537e85a79f4d030da53376ae
bakNa2t <ok2.lock@gmail.com>
2025-04-16 11:52:59 +0300

    Create StudioSidebar

---
+++ src/modules/studio/ui/components/studio-sidebar/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { LogOutIcon, VideoIcon } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+
+export const StudioSidebar = () => {
+  const pathname = usePathname();
+
+  return (
+    <Sidebar className="pt-16 border z-40" collapsible="icon">
+      <SidebarContent className="bg-background">
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                tooltip="Content"
+                isActive={pathname === "/studio"}
+                asChild
+              >
+                <Link href="/studio">
+                  <VideoIcon className="size-5" />
+                  <span className="text-sm">Content</span>
+                </Link>
+              </SidebarMenuButton>
+
+              <Separator className="my-2" />
+
+              <SidebarMenuButton tooltip="Exit studio" asChild>
+                <Link href="/">
+                  <LogOutIcon className="size-5" />
+                  <span className="text-sm">Exit studio</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
