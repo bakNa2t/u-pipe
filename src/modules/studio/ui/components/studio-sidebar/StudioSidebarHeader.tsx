7a3e07fe2031633fda629014b7d28be1263b8c75
bakNa2t <ok2.lock@gmail.com>
2025-04-16 14:26:46 +0300

    Add user's avatar Skeleton mock

--- src/modules/studio/ui/components/studio-sidebar/StudioSidebarHeader.tsx
+++ src/modules/studio/ui/components/studio-sidebar/StudioSidebarHeader.tsx
@@ -3,11 +3,22 @@ import { useUser } from "@clerk/nextjs";
 
 import { SidebarHeader } from "@/components/ui/sidebar";
 import { UserAvatar } from "@/components/user-avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const StudioSidebarHeader = () => {
   const { user } = useUser();
 
-  if (!user) return null;
+  if (!user) {
+    return (
+      <SidebarHeader className="flex items-center justify-center pb-4">
+        <Skeleton className="size-[112px] rounded-full" />
+        <div className="flex flex-col items-center gap-y-1 mt-2">
+          <Skeleton className="w-[80px] h-4" />
+          <Skeleton className="w-[100px] h-4" />
+        </div>
+      </SidebarHeader>
+    );
+  }
 
   return (
     <SidebarHeader className="flex items-center justify-center pb-4">
@@ -19,7 +30,7 @@ export const StudioSidebarHeader = () => {
         />
       </Link>
 
-      <div className="flex flex-col items-center mt-2">
+      <div className="flex flex-col items-center gap-y-1 mt-2">
         <p className="text-sm font-medium">Your profile</p>
         <p className="text-xs text-muted-foreground">{user.fullName}</p>
       </div>
