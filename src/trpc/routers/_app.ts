7ebd49ffea884039d3b1c04b0cb745209f9d9f37
bakNa2t <ok2.lock@gmail.com>
2025-04-17 10:50:18 +0300

    Add studio to appRouter

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,10 @@
 import { categoriesRouter } from "@/modules/categories/server/procedures";
+import { studioRouter } from "@/modules/studio/server/procedures";
+
 import { createTRPCRouter } from "../init";
+
 export const appRouter = createTRPCRouter({
+  studio: studioRouter,
   categories: categoriesRouter,
 });
 // export type definition of API
