eb09fd72b28b260e0b0a06e12ae3dab87ec34305
bakNa2t <ok2.lock@gmail.com>
2025-04-18 15:03:41 +0300

    Add Loader2Icon if isPending and useUtils

--- src/modules/studio/ui/components/StudioUploadModal.tsx
+++ src/modules/studio/ui/components/StudioUploadModal.tsx
@@ -1,17 +1,26 @@
 "use client";
 
-import { PlusIcon } from "lucide-react";
+import { Loader2Icon, PlusIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
 import { trpc } from "@/trpc/client";
 
 export const StudioUploadModal = () => {
-  const create = trpc.videos.create.useMutation();
+  const utils = trpc.useUtils();
+  const create = trpc.videos.create.useMutation({
+    onSuccess: () => {
+      utils.studio.getMany.invalidate();
+    },
+  });
 
   return (
-    <Button variant="secondary" onClick={() => create.mutate()}>
-      <PlusIcon />
+    <Button
+      variant="secondary"
+      onClick={() => create.mutate()}
+      disabled={create.isPending}
+    >
+      {create.isPending ? <Loader2Icon /> : <PlusIcon />}
       Create
     </Button>
   );
