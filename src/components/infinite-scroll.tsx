470174c894227be87b83defd82c12b068751aae5
bakNa2t <ok2.lock@gmail.com>
2025-04-21 13:58:41 +0300

    Add useIntersectionObserver to bind targetRef

--- src/components/infinite-scroll.tsx
+++ src/components/infinite-scroll.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+
+import { useIntersectionObserver } from "@/hooks/use-intersection-observer";
+
 interface InfiniteScrollProps {
   isManual?: boolean;
   hasNextPage: boolean;
@@ -5,8 +9,32 @@ interface InfiniteScrollProps {
   fetchNextPage: () => void;
 }
 
-export const InfiniteScroll = ({}: InfiniteScrollProps) => {
+export const InfiniteScroll = ({
+  isManual,
+  hasNextPage,
+  isFetchingNextPage,
+  fetchNextPage,
+}: InfiniteScrollProps) => {
+  const { targetRef, isIntersecting } = useIntersectionObserver({
+    threshold: 0.5,
+    rootMargin: "100px",
+  });
+
+  useEffect(() => {
+    if (isIntersecting && hasNextPage && !isFetchingNextPage && !isManual) {
+      fetchNextPage();
+    }
+  }, [
+    isIntersecting,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+    isManual,
+  ]);
+
   return (
-    <div className="flex flex-col items-center gap-4 p-4">InfiniteScroll</div>
+    <div className="flex flex-col items-center gap-4 p-4">
+      <div ref={targetRef} />
+    </div>
   );
 };
