5ea9fb929805e2eada1504d6bd0a368669b6a2b2
bakNa2t <ok2.lock@gmail.com>
2025-04-23 10:52:31 +0300

    Add shadcn Table to display videos content

--- src/modules/studio/ui/sections/VideosSection.tsx
+++ src/modules/studio/ui/sections/VideosSection.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import Link from "next/link";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-import { trpc } from "@/trpc/client";
-
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
+import { trpc } from "@/trpc/client";
 import { DEFAULT_LIMIT } from "@/constants";
 
 export const VideosSection = () => {
@@ -20,7 +28,7 @@ export const VideosSection = () => {
 };
 
 const VideosSectionSuspense = () => {
-  const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     {
       limit: DEFAULT_LIMIT,
     },
@@ -31,7 +39,44 @@ const VideosSectionSuspense = () => {
 
   return (
     <div>
-      {JSON.stringify(data)}
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[510px] pl-6">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <Link
+                  href={`/studio/videos/${video.id}`}
+                  key={video.id}
+                  legacyBehavior
+                >
+                  <TableRow className="cursor-pointer">
+                    <TableCell>{video.title}</TableCell>
+                    <TableCell>visibility</TableCell>
+                    <TableCell>status</TableCell>
+                    <TableCell>date</TableCell>
+                    <TableCell>views</TableCell>
+                    <TableCell>comments</TableCell>
+                    <TableCell>likes</TableCell>
+                  </TableRow>
+                </Link>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+
       <InfiniteScroll
         isManual
         hasNextPage={query.hasNextPage}
