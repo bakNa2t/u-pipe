6db247985b0548466dfb4bd4eef537ea75417027
bakNa2t <ok2.lock@gmail.com>
2025-04-23 16:31:23 +0300

    Add mux's upload url

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,10 +1,20 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
 import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user;
+
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+        mp4_support: "standard",
+      },
+      cors_origin: "*",
+    });
 
     const [video] = await db
       .insert(videos)
@@ -16,6 +26,7 @@ export const videosRouter = createTRPCRouter({
 
     return {
       video: video,
+      url: upload.url,
     };
   }),
 });
