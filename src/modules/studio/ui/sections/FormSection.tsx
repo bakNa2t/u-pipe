f15c6b54c728574e96f75ed64b4784bbdc1db222
bakNa2t <ok2.lock@gmail.com>
2025-04-28 12:09:24 +0300

    Wrap comp to react suspense

--- src/modules/studio/ui/sections/FormSection.tsx
+++ src/modules/studio/ui/sections/FormSection.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface FormSectionProps {
   videoId: string;
 }
 
 export const FormSection = ({ videoId }: FormSectionProps) => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <FormSectionSuspense videoId={videoId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
   const [video] = trpc.studio.getOne.useSuspenseQuery({ id: videoId });
 
   return <div>{JSON.stringify(video)}</div>;
