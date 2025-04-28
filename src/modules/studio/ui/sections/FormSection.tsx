294b80e0b63d6e2cb696edf976c73c2ed9946ced
bakNa2t <ok2.lock@gmail.com>
2025-04-28 13:09:36 +0300

    Add save and dropdown delete btn

--- src/modules/studio/ui/sections/FormSection.tsx
+++ src/modules/studio/ui/sections/FormSection.tsx
@@ -3,6 +3,15 @@
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { MoreVerticalIcon, TrashIcon } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 interface FormSectionProps {
   videoId: string;
@@ -21,5 +30,35 @@ export const FormSection = ({ videoId }: FormSectionProps) => {
 const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
   const [video] = trpc.studio.getOne.useSuspenseQuery({ id: videoId });
 
-  return <div>{JSON.stringify(video)}</div>;
+  return (
+    <div className="flex items-center justify-between mb-6">
+      <div>
+        <h1 className="text-2xl font-bold">Video details</h1>
+        <p className="text-xs text-muted-foreground">
+          Mange your video details
+        </p>
+      </div>
+
+      <div className="flex items-center gap-x-2">
+        <Button type="submit" disabled={false}>
+          Save
+        </Button>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <MoreVerticalIcon />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>
+              <TrashIcon className="size-4 mr-2" />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
 };
