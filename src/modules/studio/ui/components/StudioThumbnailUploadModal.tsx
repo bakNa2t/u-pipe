eff85f64d95993fdbad3c278ac59a9292b47b141
bakNa2t <ok2.lock@gmail.com>
2025-04-30 14:41:20 +0300

    Add UploadDropzone comp and onUploadComplete fn

--- src/modules/studio/ui/components/StudioThumbnailUploadModal.tsx
+++ src/modules/studio/ui/components/StudioThumbnailUploadModal.tsx
@@ -1,4 +1,7 @@
+import { trpc } from "@/trpc/client";
+
 import { ResponsiveModal } from "@/components/responsive-modal";
+import { UploadDropzone } from "@/lib/uploadthing";
 
 interface StudioThumbnailUploadModal {
   videoId: string;
@@ -11,13 +14,24 @@ export const StudioThumbnailUploadModal = ({
   open,
   onOpenChange,
 }: StudioThumbnailUploadModal) => {
+  const utils = trpc.useUtils();
+
+  const onUploadComplete = () => {
+    utils.studio.getOne.invalidate({ id: videoId });
+    onOpenChange(false);
+  };
+
   return (
     <ResponsiveModal
       title="Upload a thumbnail"
       open={open}
       onOpenChange={onOpenChange}
     >
-      <p>It&apos;s thumbnail ID: {videoId ? videoId : "NO ID FOUND"}</p>
+      <UploadDropzone
+        endpoint="thumbnailUploader"
+        input={{ videoId }}
+        onClientUploadComplete={onUploadComplete}
+      />
     </ResponsiveModal>
   );
 };
