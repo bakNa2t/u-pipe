00c6f03c8c11bac86a00723ad171c697ae15a9d3
bakNa2t <ok2.lock@gmail.com>
2025-05-02 10:35:06 +0300

    Add upstash's serve get-video to retrive existing video from db

--- src/app/api/videos/workflows/title/route.ts
+++ src/app/api/videos/workflows/title/route.ts
@@ -1,6 +1,33 @@
 import { serve } from "@upstash/workflow/nextjs";
 
+import { db } from "@/db";
+import { and, eq } from "drizzle-orm";
+import { videos } from "@/db/schema";
+
+interface InputType {
+  userId: string;
+  videoId: string;
+}
+
 export const { POST } = serve(async (context) => {
+  const input = context.requestPayload as InputType;
+  const { userId, videoId } = input;
+
+  const video = context.run("get-video", async () => {
+    const [existingVideo] = await db
+      .select()
+      .from(videos)
+      .where(and(eq(videos.id, videoId), eq(videos.userId, userId)));
+
+    if (!existingVideo) {
+      throw new Error();
+    }
+
+    return existingVideo;
+  });
+
+  console.log(video);
+
   await context.run("initial-step", () => {
     console.log("initial step ran");
   });
