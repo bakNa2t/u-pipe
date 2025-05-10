40d801826f17ef965333a6181fdb947d0a36f179
bakNa2t <ok2.lock@gmail.com>
2025-05-10 12:32:03 +0300

    Create VideoDescription

---
+++ src/modules/videos/ui/components/VideoDescription.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+interface VideoDescriptionProps {
+  compactViews: string;
+  expandedViews: string;
+  compactDate: string;
+  expandedDate: string;
+  description?: string | null;
+}
+
+export const VideoDescription = ({
+  compactViews,
+  expandedViews,
+  compactDate,
+  expandedDate,
+  description,
+}: VideoDescriptionProps) => {
+  const [isExpanded, seIsExpanded] = useState(false);
+
+  return (
+    <div className="bg-secondary/50 rounded-xl p-3 cursor-pointer hover:bg-secondary/70 transition">
+      <div className="flex gap-2 text-sm mb-2">
+        <span className="font-medium">
+          {isExpanded ? expandedViews : compactViews} views
+        </span>
+        <span className="font-medium">
+          {isExpanded ? expandedDate : compactDate}
+        </span>
+      </div>
+
+      <div className="relative">
+        <p
+          className={cn(
+            "text-sm whitespace-pre-wrap",
+            !isExpanded && "line-clamp-2"
+          )}
+        >
+          {description || "No description"}
+        </p>
+      </div>
+    </div>
+  );
+};
