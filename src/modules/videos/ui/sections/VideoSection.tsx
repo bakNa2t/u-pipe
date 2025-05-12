9bcd5bf9438fff9f98aecd4b181c7aebfc348260
bakNa2t <ok2.lock@gmail.com>
2025-05-12 16:43:03 +0300

    Add trpc create view mutation and handlePlay fn

--- src/modules/videos/ui/sections/VideoSection.tsx
+++ src/modules/videos/ui/sections/VideoSection.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
+import { useAuth } from "@clerk/nextjs";
 import { ErrorBoundary } from "react-error-boundary";
 
 import { cn } from "@/lib/utils";
+import { trpc } from "@/trpc/client";
 
 import { VideoPlayer } from "../components/VideoPlayer";
 import { VideoBanner } from "../components/VideoBanner";
@@ -25,7 +26,21 @@ export const VideoSection = ({ videoId }: VideoSectionProps) => {
 };
 
 const VideoSectionSuspense = ({ videoId }: VideoSectionProps) => {
+  const { isSignedIn } = useAuth();
+
+  const utils = trpc.useUtils();
   const [video] = trpc.videos.getOne.useSuspenseQuery({ id: videoId });
+  const createView = trpc.videoViews.create.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+    },
+  });
+
+  const handlePlay = () => {
+    if (!isSignedIn) return;
+
+    createView.mutate({ videoId });
+  };
 
   return (
     <>
@@ -39,7 +54,7 @@ const VideoSectionSuspense = ({ videoId }: VideoSectionProps) => {
           autoPlay
           playbackId={video.muxPlaybackId}
           thumbnailUrl={video.thumbnailUrl}
-          onPlay={() => {}}
+          onPlay={handlePlay}
         />
       </div>
 
