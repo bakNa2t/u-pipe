7aeec8d48635d59f9c4d83b883866381cdd2e54f
bakNa2t <ok2.lock@gmail.com>
2025-05-14 14:26:50 +0300

    Create useSubscription hook

---
+++ src/modules/subscriptions/hooks/useSubscription.ts
@@ -0,0 +1,68 @@
+import { useClerk } from "@clerk/nextjs";
+import { toast } from "sonner";
+
+import { trpc } from "@/trpc/client";
+
+interface UseSubscriptionProps {
+  userId: string;
+  isSubscribed: boolean;
+  fromVideoId: string;
+}
+
+export const useSubscription = ({
+  userId,
+  isSubscribed,
+  fromVideoId,
+}: UseSubscriptionProps) => {
+  const clerk = useClerk();
+  const utils = trpc.useUtils();
+
+  const subscribe = trpc.subsriptions.create.useMutation({
+    onSuccess: () => {
+      toast.success("Subscribed successfully");
+
+      if (fromVideoId) {
+        utils.videos.getOne.invalidate({ id: fromVideoId });
+      }
+    },
+    onError: (error) => {
+      toast.error("Something went wrong");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
+  const unsubscribe = trpc.subsriptions.remove.useMutation({
+    onSuccess: () => {
+      toast.success("Unsubscribed successfully");
+
+      if (fromVideoId) {
+        utils.videos.getOne.invalidate({ id: fromVideoId });
+      }
+    },
+    onError: (error) => {
+      toast.error("Something went wrong");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
+  const isPending = subscribe.isPending || unsubscribe.isPending;
+
+  const onClick = () => {
+    if (isSubscribed) {
+      unsubscribe.mutate({ userId });
+    } else {
+      subscribe.mutate({ userId });
+    }
+  };
+
+  return {
+    isPending,
+    onClick,
+  };
+};
