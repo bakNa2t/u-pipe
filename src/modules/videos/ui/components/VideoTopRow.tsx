e9b2cb0388bee174de28d9eca599d0e1c2e0556f
bakNa2t <ok2.lock@gmail.com>
2025-05-14 17:23:23 +0300

    Add VideoTopRowSkeleton

--- src/modules/videos/ui/components/VideoTopRow.tsx
+++ src/modules/videos/ui/components/VideoTopRow.tsx
@@ -5,12 +5,37 @@ import { VideoOwner } from "./VideoOwner";
 import { VideoMenu } from "./VideoMenu";
 import { VideoReactions } from "./VideoReactions";
 import { VideoDescription } from "./VideoDescription";
+import { Skeleton } from "@/components/ui/skeleton";
 
 import { VideoGetOneOutput } from "../../types";
 
 interface VideoTopRowProps {
   video: VideoGetOneOutput;
 }
+
+export const VideoTopRowSkeleton = () => {
+  return (
+    <div className="flex flex-col gap-4 mt-4">
+      <div className="flex flex-col gap-2">
+        <Skeleton className="w-4/5 h-6 md:w-2/5" />
+      </div>
+
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-3 w-[70%]">
+          <Skeleton className="w-10 h-10 rounded-full shrink-0" />
+
+          <div className="flex flex-col gap-2 w-full">
+            <Skeleton className="w-4/5 h-5 md:w-2/6" />
+            <Skeleton className="w-3/5 h-5 md:w-1/5" />
+          </div>
+        </div>
+
+        <Skeleton className="w-2/6 h-9 md:w-1/6 rounded-full" />
+      </div>
+      <div className="w-full h-[120px]" />
+    </div>
+  );
+};
 
 export const VideoTopRow = ({ video }: VideoTopRowProps) => {
   const compactViews = useMemo(() => {
