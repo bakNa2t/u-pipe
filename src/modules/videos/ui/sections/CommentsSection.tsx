e5076b16f5a7aa1ea5faf8b8060ec3951e9e3981
bakNa2t <ok2.lock@gmail.com>
2025-05-15 13:57:47 +0300

    Add react suspense and error boundary

--- src/modules/videos/ui/sections/CommentsSection.tsx
+++ src/modules/videos/ui/sections/CommentsSection.tsx
@@ -1,3 +1,36 @@
-export const CommentsSection = () => {
-  return <div>Comments</div>;
+"use client";
+
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { CommentForm } from "@/modules/comments/ui/components/CommentForm";
+
+import { trpc } from "@/trpc/client";
+
+interface CommentsSectionProps {
+  videoId: string;
+}
+
+export const CommentsSection = ({ videoId }: CommentsSectionProps) => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <CommentsSectionSuspense videoId={videoId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const CommentsSectionSuspense = ({ videoId }: CommentsSectionProps) => {
+  const [commnets] = trpc.comments.getManu.useSuspenseQuery({ videoId });
+  console.log(commnets);
+
+  return (
+    <div className="mt-6 ">
+      <div className="flex flex-col gap-6">
+        <h1>0 comments</h1>
+        <CommentForm videoId={videoId} onSuccess={() => {}} />
+      </div>
+    </div>
+  );
 };
