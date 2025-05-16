e31a8aa421cfba417c9e4be6ab85832f45a6d205
bakNa2t <ok2.lock@gmail.com>
2025-05-16 12:28:25 +0300

    Add innerJoin query for getMany procedure

--- src/modules/comments/server/procedure.ts
+++ src/modules/comments/server/procedure.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
+import { eq, getTableColumns } from "drizzle-orm";
 
 import { db } from "@/db";
-import { comments } from "@/db/schema";
+import { comments, users } from "@/db/schema";
 import {
   baseProcedure,
   createTRPCRouter,
   protectedProcedure,
 } from "@/trpc/init";
-import { eq } from "drizzle-orm";
 
 export const commentsRouter = createTRPCRouter({
   create: protectedProcedure
@@ -38,9 +38,13 @@ export const commentsRouter = createTRPCRouter({
       const { videoId } = input;
 
       const data = await db
-        .select()
+        .select({
+          ...getTableColumns(comments),
+          user: users,
+        })
         .from(comments)
-        .where(eq(comments.videoId, videoId));
+        .where(eq(comments.videoId, videoId))
+        .innerJoin(users, eq(comments.userId, users.id));
 
       return data;
     }),
