67c83ae2fa3275327ee5ca76d1b0fb4eea353931
bakNa2t <ok2.lock@gmail.com>
2025-05-16 13:06:56 +0300

    Add comment details and value

--- src/modules/comments/ui/components/CommentItem.tsx
+++ src/modules/comments/ui/components/CommentItem.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
 
 import { UserAvatar } from "@/components/user-avatar";
 
@@ -19,6 +20,24 @@ export const CommentItem = ({ comment }: CommentItemProps) => {
             name={comment.user.name}
           />
         </Link>
+
+        <div className="flex-1 min-w-0">
+          <Link href={`/users/${comment.userId}`}>
+            <div className="flex items-center gap-2 mb-0.5">
+              <span className="text-sm font-medium pb-0.5">
+                {comment.user.name}
+              </span>
+
+              <span className="text-xs text-muted-foreground">
+                {formatDistanceToNow(comment.createdAt, {
+                  addSuffix: true,
+                })}
+              </span>
+            </div>
+          </Link>
+
+          <p className="text-sm">{comment.value}</p>
+        </div>
       </div>
     </div>
   );
