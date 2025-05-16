5c50b510f47cf1ecfad9b62c4f291a19a1ec8df8
bakNa2t <ok2.lock@gmail.com>
2025-05-16 11:50:53 +0300

    Add trpc create comment mutation and shadcn ui Form

--- src/modules/comments/ui/components/CommentForm.tsx
+++ src/modules/comments/ui/components/CommentForm.tsx
@@ -1,8 +1,22 @@
-import { useUser } from "@clerk/nextjs";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { useUser, useClerk } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
 
 import { UserAvatar } from "@/components/user-avatar";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { trpc } from "@/trpc/client";
+import { commentInsertSchema } from "@/db/schema";
 
 interface CommentFormProps {
   videoId: string;
@@ -11,27 +25,75 @@ interface CommentFormProps {
 
 export const CommentForm = ({ videoId, onSuccess }: CommentFormProps) => {
   const { user } = useUser();
+  const clerk = useClerk();
+  const utils = trpc.useUtils();
+
+  const create = trpc.comments.create.useMutation({
+    onSuccess: () => {
+      utils.comments.getManu.invalidate({ videoId });
+      form.reset();
+
+      toast.success("Comment added");
+      onSuccess?.();
+    },
+    onError: (error) => {
+      toast.error("Something went wrong");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
+  const form = useForm<z.infer<typeof commentInsertSchema>>({
+    resolver: zodResolver(commentInsertSchema.omit({ userId: true })),
+    defaultValues: {
+      videoId,
+      value: "",
+    },
+  });
+
+  const handleSubmit = (values: z.infer<typeof commentInsertSchema>) => {
+    create.mutate(values);
+  };
 
   return (
-    <form className="flex gap-4 gruop">
-      <UserAvatar
-        size="lg"
-        imageUrl={user?.imageUrl ?? "/default-avatar.svg"}
-        name={user?.username || "User"}
-      />
-
-      <div className="flex-1">
-        <Textarea
-          placeholder="Add a comment..."
-          className="resize-none bg-transparent overflow-hidden min-h-0"
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="flex gap-4 gruop"
+      >
+        <UserAvatar
+          size="lg"
+          imageUrl={user?.imageUrl ?? "/default-avatar.svg"}
+          name={user?.username || "User"}
         />
 
-        <div className="flex justify-end gap-2 mt-2">
-          <Button type="submit" size="sm">
-            Comment
-          </Button>
+        <div className="flex-1">
+          <FormField
+            name="value"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    placeholder="Add a comment..."
+                    className="resize-none bg-transparent overflow-hidden min-h-0"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-end gap-2 mt-2">
+            <Button type="submit" size="sm" disabled={create.isPending}>
+              Comment
+            </Button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </Form>
   );
 };
