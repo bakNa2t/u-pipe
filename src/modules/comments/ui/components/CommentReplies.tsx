52702db5d2abe6d337303ddf73359bc913030ee0
bakNa2t <ok2.lock@gmail.com>
2025-05-21 09:43:28 +0300

    Create CommentReplies

---
+++ src/modules/comments/ui/components/CommentReplies.tsx
@@ -0,0 +1,42 @@
+import { trpc } from "@/trpc/client";
+
+import { DEFAULT_LIMIT } from "@/constants";
+import { Loader2Icon } from "lucide-react";
+import { CommentItem } from "./CommentItem";
+
+interface CommentRepliesProps {
+  parentId: string;
+  videoId: string;
+}
+
+export const CommentReplies = ({ parentId, videoId }: CommentRepliesProps) => {
+  const { data, isLoading } = trpc.comments.getMany.useInfiniteQuery(
+    {
+      parentId,
+      videoId,
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  return (
+    <div className="pl-14">
+      <div className="flex flex-col gap-4 mt-2">
+        {isLoading && (
+          <div className="flex items-center justify-center">
+            <Loader2Icon className="size-6 animate-spin text-muted-foreground" />
+          </div>
+        )}
+
+        {!isLoading &&
+          data?.pages
+            .flatMap((page) => page.items)
+            .map((comment) => (
+              <CommentItem key={comment.id} comment={comment} variant="reply" />
+            ))}
+      </div>
+    </div>
+  );
+};
