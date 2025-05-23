b8414ccfc42fa1ab1dfc886bd2ff166fdac6a409
bakNa2t <ok2.lock@gmail.com>
2025-05-23 10:36:38 +0300

    Create VideoRowCard

---
+++ src/modules/videos/ui/components/VideoRowCard.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import { cva, VariantProps } from "class-variance-authority";
+
+import { VideoGetManyOutput } from "../../types";
+import { VideoThumbnail } from "./VideoThumbnail";
+
+const videoRowCardVariants = cva("group flex min-w-0", {
+  variants: {
+    size: {
+      default: "gap-4",
+      compact: "gap-2",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+const thumbnailVariants = cva("relative flex-none", {
+  variants: {
+    size: {
+      default: "w-[38%]",
+      compact: "w-[168px]",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+interface VideoRowCardProps extends VariantProps<typeof videoRowCardVariants> {
+  data: VideoGetManyOutput["items"][number];
+  onRemove?: () => void;
+}
+
+export const VideoRowCard = ({ data, size, onRemove }: VideoRowCardProps) => {
+  return (
+    <div className={videoRowCardVariants({ size })}>
+      <Link href={`/videos/${data.id}`} className={thumbnailVariants({ size })}>
+        <VideoThumbnail
+          imageUrl={data.thumbnailUrl}
+          previewUrl={data.previewUrl}
+          title={data.title}
+          duration={data.duration}
+        />
+      </Link>
+    </div>
+  );
+};
