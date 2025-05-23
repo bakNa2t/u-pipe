796f8a7bb3938a094f453b9dbfedf59b33f19090
bakNa2t <ok2.lock@gmail.com>
2025-05-23 11:13:05 +0300

    Add video details for default screen size

--- src/modules/videos/ui/components/VideoRowCard.tsx
+++ src/modules/videos/ui/components/VideoRowCard.tsx
@@ -1,8 +1,17 @@
 import Link from "next/link";
 import { cva, VariantProps } from "class-variance-authority";
 
-import { VideoGetManyOutput } from "../../types";
+import { UserAvatar } from "@/components/user-avatar";
 import { VideoThumbnail } from "./VideoThumbnail";
+import { UserInfo } from "@/modules/users/ui/components/UserInfo";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { cn } from "@/lib/utils";
+import { VideoGetManyOutput } from "../../types";
 
 const videoRowCardVariants = cva("group flex min-w-0", {
   variants: {
@@ -44,6 +53,58 @@ export const VideoRowCard = ({ data, size, onRemove }: VideoRowCardProps) => {
           duration={data.duration}
         />
       </Link>
+
+      {/* Video details */}
+      <div className="flex-1 min-w-0">
+        <div className="flex justify-between gap-x-2">
+          <Link href={`/videos/${data.id}`} className="flex-1 min-w-0">
+            <h3
+              className={cn(
+                "font-medium line-clamp-2",
+                size === "compact" ? "text-sm" : "text-base"
+              )}
+            >
+              {data.title}
+            </h3>
+
+            {size === "default" && (
+              <p className="text-sm text-muted-foreground mt-1">
+                {data.viewCount} views &bull; {data.likeCount} likes
+              </p>
+            )}
+
+            {size === "default" && (
+              <>
+                <div className="flex items-center gap-2 my-3">
+                  <UserAvatar
+                    size="sm"
+                    imageUrl={data.user.imageUrl}
+                    name={data.user.name}
+                  />
+
+                  <UserInfo size="sm" name={data.user.name} />
+                </div>
+
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <p className="w-fit text-xs text-muted-foreground line-clamp-2">
+                      {data.description ?? "No description"}
+                    </p>
+                  </TooltipTrigger>
+
+                  <TooltipContent
+                    side="bottom"
+                    align="center"
+                    className="bg-black/70"
+                  >
+                    <p>From the video description</p>
+                  </TooltipContent>
+                </Tooltip>
+              </>
+            )}
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
