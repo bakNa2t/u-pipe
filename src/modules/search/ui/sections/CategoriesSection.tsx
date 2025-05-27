5fcae2fea4b6e20a62647e46ab9fcc333ef7815c
bakNa2t <ok2.lock@gmail.com>
2025-05-27 13:50:41 +0300

    Create search CategoriesSection

---
+++ src/modules/search/ui/sections/CategoriesSection.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Suspense } from "react";
+import { useRouter } from "next/navigation";
+import { trpc } from "@/trpc/client";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { FilterCarousel } from "@/components/filter-carousel";
+
+interface CategoriesSectionProps {
+  categoryId?: string;
+}
+
+export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
+  return (
+    <Suspense fallback={<CategoriesSkeleton />}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <CategoriesSectionSuspense categoryId={categoryId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
+  const router = useRouter();
+  const [categories] = trpc.categories.getMany.useSuspenseQuery();
+
+  const data = categories.map((category) => ({
+    value: category.id,
+    label: category.name,
+  }));
+
+  const onSelectAction = (value: string | null) => {
+    const url = new URL(window.location.href);
+
+    if (value) {
+      url.searchParams.set("categoryId", value);
+    } else {
+      url.searchParams.delete("categoryId");
+    }
+
+    router.push(url.toString());
+  };
+
+  return (
+    <FilterCarousel
+      value={categoryId}
+      data={data}
+      onSelectAction={onSelectAction}
+    />
+  );
+};
+
+const CategoriesSkeleton = () => {
+  return <FilterCarousel isLoading data={[]} onSelectAction={() => {}} />;
+};
