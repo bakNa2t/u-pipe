de95fc92a18addeaa7748d9126287c0c962e87ed
bakNa2t <ok2.lock@gmail.com>
2025-05-27 13:43:41 +0300

    Create search ResultsSection

---
+++ src/modules/search/ui/sections/ResultsSection.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { trpc } from "@/trpc/client";
+
+import { VideoGridCard } from "@/modules/videos/ui/components/VideoGridCard";
+import { VideoRowCard } from "@/modules/videos/ui/components/VideoRowCard";
+
+import { useIsMobile } from "@/hooks/use-mobile";
+import { DEFAULT_LIMIT } from "@/constants";
+
+interface ResultsSectionProps {
+  query: string | undefined;
+  categoryId: string | undefined;
+}
+
+export const ResultsSection = ({ query, categoryId }: ResultsSectionProps) => {
+  const isMobile = useIsMobile();
+  const [results, resultQuery] = trpc.search.getMany.useSuspenseInfiniteQuery(
+    {
+      query,
+      categoryId,
+      limit: DEFAULT_LIMIT,
+    },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+  return (
+    <>
+      {isMobile ? (
+        <div className="flex flex-col gap-y-10">
+          {results.pages
+            .flatMap((page) => page.items)
+            .map((video) => (
+              <VideoGridCard key={video.id} data={video} />
+            ))}
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          {results.pages
+            .flatMap((page) => page.items)
+            .map((video) => (
+              <VideoRowCard key={video.id} data={video} />
+            ))}
+        </div>
+      )}
+    </>
+  );
+};
