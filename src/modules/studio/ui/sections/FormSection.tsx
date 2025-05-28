c7e4e0789ee084d255d331086d368762c7b076e5
bakNa2t <ok2.lock@gmail.com>
2025-05-28 11:45:15 +0300

    Add NEXT_PUBLIK_APP_URL to .env and update comps constants

--- src/modules/studio/ui/sections/FormSection.tsx
+++ src/modules/studio/ui/sections/FormSection.tsx
@@ -226,7 +226,7 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
     update.mutateAsync(data);
   };
 
-  const fullUrl = `${APP_URL || `http://localhost:3000/videos/${videoId}`}`;
+  const fullUrl = `${APP_URL}/videos/${videoId}`;
 
   const onCopy = async () => {
     await navigator.clipboard.writeText(fullUrl);
