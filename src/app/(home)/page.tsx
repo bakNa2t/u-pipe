282122fa9c4fcdb14db5bdebbfdfc4b52c4bce4d
bakNa2t <ok2.lock@gmail.com>
2025-05-30 10:40:27 +0300

    Add trpc' videos getMany prefetchInfinite

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import { HydrateClient, trpc } from "@/trpc/server";
 
 import { HomeView } from "@/modules/home/ui/views/HomeView";
 
+import { DEFAULT_LIMIT } from "@/constants";
+
 export const dynamic = "force-dynamic";
 
 interface PageProps {
@@ -14,6 +16,11 @@ const Page = async ({ searchParams }: PageProps) => {
   const { categoryId } = await searchParams;
 
   void trpc.categories.getMany.prefetch();
+  void trpc.videos.getMany.prefetchInfinite({
+    categoryId,
+    limit: DEFAULT_LIMIT,
+  });
+
   return (
     <div>
       <h1 className="text-2xl font-semibold tracking-tight text-center">
