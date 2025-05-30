bf62cc0f0430c1088b635db370d4a9f9a886f049
bakNa2t <ok2.lock@gmail.com>
2025-05-30 10:36:55 +0300

    Add HomeVideosSection to view

--- src/modules/home/ui/views/HomeView.tsx
+++ src/modules/home/ui/views/HomeView.tsx
@@ -1,4 +1,5 @@
 import { CategoriesSection } from "../sections/CategoriesSection";
+import { HomeVideosSection } from "../sections/HomeVideosSection";
 
 interface HomeViewProps {
   categoryId?: string;
@@ -8,6 +9,7 @@ export const HomeView = ({ categoryId }: HomeViewProps) => {
   return (
     <div className="flex flex-col gap-y-6 max-w-[2400px] mx-auto mb-10 px-4 pt-2.5">
       <CategoriesSection categoryId={categoryId} />
+      <HomeVideosSection categoryId={categoryId} />
     </div>
   );
 };
