45b01fc1817e3e91d9d9e13dae00da21ee6a8116
bakNa2t <ok2.lock@gmail.com>
2025-06-02 14:47:40 +0300

    Update HistoryVideosSectionSkeleton style

--- src/modules/playlists/ui/sections/HistoryVideosSection.tsx
+++ src/modules/playlists/ui/sections/HistoryVideosSection.tsx
@@ -7,7 +7,10 @@ import {
   VideoGridCard,
   VideoGridCardSkeleton,
 } from "@/modules/videos/ui/components/VideoGridCard";
-import { VideoRowCard } from "@/modules/videos/ui/components/VideoRowCard";
+import {
+  VideoRowCard,
+  VideoRowCardSkeleton,
+} from "@/modules/videos/ui/components/VideoRowCard";
 import { InfiniteScroll } from "@/components/infinite-scroll";
 
 import { trpc } from "@/trpc/client";
@@ -25,10 +28,17 @@ export const HistoryVideosSection = () => {
 
 const HistoryVideosSectionSkeleton = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [media(min-width:1920px)]:grid-cols-5 [media(min-width:2200px)]:grid-cols-6">
-      {Array.from({ length: 18 }).map((_, index) => (
-        <VideoGridCardSkeleton key={index} />
-      ))}
+    <div>
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+        {Array.from({ length: 18 }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {Array.from({ length: 18 }).map((_, index) => (
+          <VideoRowCardSkeleton key={index} size="compact" />
+        ))}
+      </div>
     </div>
   );
 };
@@ -57,7 +67,7 @@ const HistoryVideosSectionSuspense = () => {
         {videos.pages
           .flatMap((page) => page.items)
           .map((video) => (
-            <VideoRowCard key={video.id} data={video} />
+            <VideoRowCard key={video.id} data={video} size="compact" />
           ))}
       </div>
 
