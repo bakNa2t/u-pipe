b7de7749f23c1c06b4c59bb663f04f926a47cbb4
bakNa2t <ok2.lock@gmail.com>
2025-06-06 12:12:05 +0300

    PlaylistsSectionSkeleton styles

--- src/modules/playlists/ui/sections/PlaylistsSection.tsx
+++ src/modules/playlists/ui/sections/PlaylistsSection.tsx
@@ -3,11 +3,11 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-import { VideoGridCardSkeleton } from "@/modules/videos/ui/components/VideoGridCard";
-import { VideoRowCardSkeleton } from "@/modules/videos/ui/components/VideoRowCard";
 import { InfiniteScroll } from "@/components/infinite-scroll";
-
-import { PlaylistGridCard } from "../components/playlist-grid-card";
+import {
+  PlaylistGridCard,
+  PlaylistGridCardSkeleton,
+} from "../components/playlist-grid-card";
 
 import { trpc } from "@/trpc/client";
 import { DEFAULT_LIMIT } from "@/constants";
@@ -24,20 +24,30 @@ export const PlaylistsSection = () => {
 
 const PlaylistsSectionSkeleton = () => {
   return (
-    <div>
-      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
-        {Array.from({ length: 18 }).map((_, index) => (
-          <VideoGridCardSkeleton key={index} />
-        ))}
-      </div>
-      <div className="hidden flex-col gap-4 md:flex">
-        {Array.from({ length: 18 }).map((_, index) => (
-          <VideoRowCardSkeleton key={index} size="compact" />
-        ))}
-      </div>
+    <div className="grid grid-cols-1 gap-4 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [media(min-width:1920px)]:grid-cols-5 [media(min-width:2200px)]:grid-cols-6">
+      {Array.from({ length: 18 }).map((_, index) => (
+        <PlaylistGridCardSkeleton key={index} />
+      ))}
     </div>
   );
 };
+
+// const PlaylistsSectionSkeleton = () => {
+//   return (
+//     <div>
+//       <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+//         {Array.from({ length: 18 }).map((_, index) => (
+//           <VideoGridCardSkeleton key={index} />
+//         ))}
+//       </div>
+//       <div className="hidden flex-col gap-4 md:flex">
+//         {Array.from({ length: 18 }).map((_, index) => (
+//           <VideoRowCardSkeleton key={index} size="compact" />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
 const PlaylistsSectionSuspense = () => {
   const [playlists, query] = trpc.playlists.getMany.useSuspenseInfiniteQuery(
