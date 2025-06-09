b1e78f9ee4df29c5093b5bef9bf30f3a65410e85
bakNa2t <ok2.lock@gmail.com>
2025-06-09 14:49:21 +0300

    Add trpc's  playlist prefetch and remove mutation

--- src/modules/playlists/ui/sections/PlaylistHeaderSection.tsx
+++ src/modules/playlists/ui/sections/PlaylistHeaderSection.tsx
@@ -1,6 +1,10 @@
 import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 import { Trash2Icon } from "lucide-react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { trpc } from "@/trpc/client";
 
 import { Button } from "@/components/ui/button";
 
@@ -23,18 +27,37 @@ export const PlaylistHeaderSection = ({
 const PlaylistHeaderSectionSuspense = ({
   playlistId,
 }: PlaylistHeaderSectionProps) => {
+  const utils = trpc.useUtils();
+  const router = useRouter();
+  const [playlist] = trpc.playlists.getOne.useSuspenseQuery({ id: playlistId });
+
+  const remove = trpc.playlists.remove.useMutation({
+    onSuccess: () => {
+      toast.success("Playlist deleted");
+      utils.playlists.getMany.invalidate();
+      router.push("/playlists");
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
+
   return (
     <div className="flex justify-between items-center">
       <div>
-        <h1 className="text-2xl font-bold">
-          Custom playlist witt id: {playlistId}
-        </h1>
+        <h1 className="text-2xl font-bold">{playlist.name}</h1>
         <p className="text-xs text-muted-foreground">
           Videos from the playlist
         </p>
       </div>
 
-      <Button variant="outline" size="icon" className="rounded-full">
+      <Button
+        variant="outline"
+        size="icon"
+        className="rounded-full"
+        onClick={() => remove.mutate({ id: playlistId })}
+        disabled={remove.isPending}
+      >
         <Trash2Icon />
       </Button>
     </div>
