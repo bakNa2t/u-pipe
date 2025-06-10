7a5eb80713480c4869173e6760934c1a89bedd97
bakNa2t <ok2.lock@gmail.com>
2025-06-10 12:05:36 +0300

    Add trpc's useUtils to invalidate procedures

--- src/modules/playlists/ui/components/PlaylistAddModal.tsx
+++ src/modules/playlists/ui/components/PlaylistAddModal.tsx
@@ -38,10 +38,12 @@ export const PlaylistAddModal = ({
   );
 
   const addVideo = trpc.playlists.addVideo.useMutation({
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast.success("Video added to playlist");
       utils.playlists.getMany.invalidate();
       utils.playlists.getManyForVideo.invalidate({ videoId });
+      utils.playlists.getOne.invalidate({ id: data.playlistId });
+      utils.playlists.getVideos.invalidate({ playlistId: data.playlistId });
     },
     onError: () => {
       toast.error("Failed to add video to playlist");
@@ -49,10 +51,12 @@ export const PlaylistAddModal = ({
   });
 
   const removeVideo = trpc.playlists.removeVideo.useMutation({
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast.success("Video removed from playlist");
       utils.playlists.getMany.invalidate();
       utils.playlists.getManyForVideo.invalidate({ videoId });
+      utils.playlists.getOne.invalidate({ id: data.playlistId });
+      utils.playlists.getVideos.invalidate({ playlistId: data.playlistId });
     },
     onError: () => {
       toast.error("Failed to remove video to playlist");
