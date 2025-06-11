b144a9768261ed73a0c6d61c8b4679ae7b352d29
bakNa2t <ok2.lock@gmail.com>
2025-06-11 09:52:29 +0300

    Create user Page

---
+++ src/app/(home)/users/[userId]/page.tsx
@@ -0,0 +1,23 @@
+import { HydrateClient, trpc } from "@/trpc/server";
+
+import { UserView } from "@/modules/users/ui/components/UserView";
+
+interface PageProps {
+  params: Promise<{
+    userId: string;
+  }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { userId } = await params;
+
+  void trpc.users.getOne.prefetch({ id: userId });
+
+  return (
+    <HydrateClient>
+      <UserView userId={userId} />
+    </HydrateClient>
+  );
+};
+
+export default Page;
