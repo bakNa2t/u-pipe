e7b26aa431eced8bffed0268b944985f46d43f5f
bakNa2t <ok2.lock@gmail.com>
2025-06-11 15:21:34 +0300

    Add usersRouter to app

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -9,6 +9,7 @@ import { commentReactionsRouter } from "@/modules/comment-reactions/server/proce
 import { suggestionsRouter } from "@/modules/suggestions/server/procedures";
 import { searchRouter } from "@/modules/search/server/procedure";
 import { playlistsRouter } from "@/modules/playlists/server/procedures";
+import { usersRouter } from "@/modules/users/server/procedure";
 
 import { createTRPCRouter } from "../init";
 
@@ -24,6 +25,7 @@ export const appRouter = createTRPCRouter({
   suggestions: suggestionsRouter,
   search: searchRouter,
   playlists: playlistsRouter,
+  users: usersRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
