8d99f36c481d74c9a778c50c83f450b0028ee0e3
bakNa2t <ok2.lock@gmail.com>
2025-06-13 09:12:15 +0300

    Add user info for desktop screen

--- src/modules/users/ui/components/UserPageInfo.tsx
+++ src/modules/users/ui/components/UserPageInfo.tsx
@@ -8,6 +8,7 @@ import { SubscriptionButton } from "@/modules/subscriptions/ui/components/Subscr
 
 import { UserGetOneOutput } from "../../types";
 import { useSubscription } from "@/modules/subscriptions/hooks/useSubscription";
+import { cn } from "@/lib/utils";
 
 interface UserPageInfoProps {
   user: UserGetOneOutput;
@@ -65,6 +66,46 @@ export const UserPageInfo = ({ user }: UserPageInfoProps) => {
             className="w-full mt-3"
           />
         )}
+      </div>
+
+      {/* for desktop screen */}
+      <div className="hidden items-start gap-4 md:flex">
+        <UserAvatar
+          size="xl"
+          imageUrl={user.imageUrl}
+          name={user.name}
+          className={cn(
+            userId === user.clerkId &&
+              "cursor-pointer hover:opacity-80 transition-opacity duration-300"
+          )}
+          onClick={() => {
+            if (user.clerkId === userId) {
+              clerk.openUserProfile();
+            }
+          }}
+        />
+
+        <div className="flex-1 min-w-0">
+          <h1 className="text-4xl font-bold">{user.name}</h1>
+          <div className="flex items-center gap-1 text-sm text-muted-foreground mt-3">
+            <span>{user.ssubscriberCount} subscribers</span>
+            <span>&bull;</span>
+            <span>{user.videoCount} videos</span>
+          </div>
+
+          {userId === user.clerkId ? (
+            <Button asChild variant="secondary" className="mt-3 rounded-full">
+              <Link href="/studio">Go to studio</Link>
+            </Button>
+          ) : (
+            <SubscriptionButton
+              disabled={isPending || !isLoaded}
+              isSubscribed={user.viewerSubscribed}
+              onClick={onClick}
+              className="mt-3"
+            />
+          )}
+        </div>
       </div>
     </div>
   );
