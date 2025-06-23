5ae483c0b66ff3a02b11d1f29087b78eaf81f01e
bakNa2t <ok2.lock@gmail.com>
2025-06-23 14:56:35 +0300

    Add next useTheme and dropdown menu to set up theme

--- src/modules/home/ui/components/home-navbar/ThemeSetModal.tsx
+++ src/modules/home/ui/components/home-navbar/ThemeSetModal.tsx
@@ -1,12 +1,14 @@
-import { MonitorCogIcon, MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+
 import { ResponsiveModal } from "@/components/responsive-modal";
+import { Button } from "@/components/ui/button";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface ThemeModeSetModalProps {
   open: boolean;
@@ -17,32 +19,37 @@ export const ThemeModeSetModal = ({
   open,
   onOpenChange,
 }: ThemeModeSetModalProps) => {
+  const { theme, setTheme } = useTheme();
+
   return (
     <ResponsiveModal
       title="Set app theme"
       open={open}
       onOpenChange={onOpenChange}
     >
-      <Select defaultValue="system">
-        <SelectTrigger>
-          <SelectValue placeholder="Select a theme" />
-        </SelectTrigger>
-
-        <SelectContent>
-          <SelectItem value="light">
-            <SunIcon className="size-4 mr-2" />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="row" className="md:px-4">
+            {theme === "light" ? (
+              <Sun className="size-4" />
+            ) : (
+              <Moon className="size-4" />
+            )}
+            <span>{theme === "light" ? "Light" : "Dark"}</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="start">
+          <DropdownMenuItem onClick={() => setTheme("light")}>
             Light
-          </SelectItem>
-          <SelectItem value="dark">
-            <MoonIcon className="size-4 mr-2" />
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
             Dark
-          </SelectItem>
-          <SelectItem value="system">
-            <MonitorCogIcon className="size-4 mr-2" />
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
             System
-          </SelectItem>
-        </SelectContent>
-      </Select>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </ResponsiveModal>
   );
 };
