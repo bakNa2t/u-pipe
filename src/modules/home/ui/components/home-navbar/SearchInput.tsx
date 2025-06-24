9681f0bf901b6fe12dded2f74b0de05b10d59544
bakNa2t <ok2.lock@gmail.com>
2025-06-24 10:47:27 +0300

    Update styles for dark theme

--- src/modules/home/ui/components/home-navbar/SearchInput.tsx
+++ src/modules/home/ui/components/home-navbar/SearchInput.tsx
@@ -51,7 +51,7 @@ const SearchInputSuspense = () => {
           onChange={(e) => setValue(e.target.value)}
           type="text"
           placeholder="Search..."
-          className="w-full pl-4 pr-12 py-2 rounded-l-full border focus:outline-none focus:border-violet-900"
+          className="w-full pl-4 pr-12 py-2 rounded-l-full border focus:outline-none focus:border-violet-900 dark:focus:border-violet-400"
         />
 
         {value && (
@@ -70,7 +70,7 @@ const SearchInputSuspense = () => {
       <button
         type="submit"
         disabled={!value.trim()}
-        className="px-5 py-2.5 rounded-r-full bg-violet-900/40 border-l-0 hover:bg-violet-900/60 disabled:opacity-50 disabled:cursor-not-allowed"
+        className="px-5 py-2.5 rounded-r-full bg-violet-900/40 dark:bg-violet-400/40 border-l-0 hover:bg-violet-900/60 dark:hover:bg-violet-600/80 disabled:opacity-50 disabled:cursor-not-allowed transition duration-300"
       >
         <SearchIcon className="size-5" />
       </button>
