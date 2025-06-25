c88a8cbe7b8cd2c6d7fb4fa4b5532efd12bf4d08
bakNa2t <ok2.lock@gmail.com>
2025-06-25 11:30:47 +0300

    Add DropdownMenu to choose lang

--- src/modules/home/ui/components/home-navbar/LanguageSetModal.tsx
+++ src/modules/home/ui/components/home-navbar/LanguageSetModal.tsx
@@ -1,11 +1,13 @@
+import { LanguagesIcon } from "lucide-react";
+
 import { ResponsiveModal } from "@/components/responsive-modal";
+import { Button } from "@/components/ui/button";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface LanguageSetModalProps {
   open: boolean;
@@ -22,16 +24,18 @@ export const LanguageSetModal = ({
       open={open}
       onOpenChange={onOpenChange}
     >
-      <Select defaultValue="system">
-        <SelectTrigger>
-          <SelectValue placeholder="Select a language" />
-        </SelectTrigger>
-
-        <SelectContent>
-          <SelectItem value="light">English</SelectItem>
-          <SelectItem value="dark">Russian</SelectItem>
-        </SelectContent>
-      </Select>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="row_fit" className="px-2 md:px-4">
+            <LanguagesIcon />
+            <span>Choose language</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="start">
+          <DropdownMenuItem>English</DropdownMenuItem>
+          <DropdownMenuItem>Russian</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </ResponsiveModal>
   );
 };
