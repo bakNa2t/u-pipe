91552fb75e62acfe1c46172b20e86a9f5ea6c809
bakNa2t <ok2.lock@gmail.com>
2025-07-01 10:54:32 +0300

    Add useTranslations to Trending/HistoryView and update masseges Home values

--- src/modules/home/ui/views/TrendingView.tsx
+++ src/modules/home/ui/views/TrendingView.tsx
@@ -1,13 +1,14 @@
+import { useTranslations } from "next-intl";
 import { TrendingVideosSection } from "../sections/TrendingVideosSection";
 
 export const TrendingView = () => {
+  const t = useTranslations("Home");
+
   return (
     <div className="flex flex-col gap-y-6 max-w-[2400px] mx-auto mb-10 px-4 pt-2.5">
       <div>
-        <h1 className="text-2xl font-bold">Trending</h1>
-        <p className="text-xs text-muted-foreground">
-          Most popular videos at the monent
-        </p>
+        <h1 className="text-2xl font-bold">{t("trending")}</h1>
+        <p className="text-xs text-muted-foreground">{t("trendingDesc")}</p>
       </div>
       <TrendingVideosSection />
     </div>
--- src/modules/playlists/ui/views/HistoryView.tsx
+++ src/modules/playlists/ui/views/HistoryView.tsx
@@ -1,13 +1,14 @@
+import { useTranslations } from "next-intl";
 import { HistoryVideosSection } from "../sections/HistoryVideosSection";
 
 export const HistoryView = () => {
+  const t = useTranslations("Home");
+
   return (
     <div className="flex flex-col gap-y-6 max-w-screen-md mx-auto mb-10 px-4 pt-2.5">
       <div>
-        <h1 className="text-2xl font-bold">History</h1>
-        <p className="text-xs text-muted-foreground">
-          All videos you have watched
-        </p>
+        <h1 className="text-2xl font-bold">{t("history")}</h1>
+        <p className="text-xs text-muted-foreground">{t("historyDesc")}</p>
       </div>
       <HistoryVideosSection />
     </div>
