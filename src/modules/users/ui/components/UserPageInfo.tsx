ae7dec24c2725064503dcb59410d8892a0cd29c0
bakNa2t <ok2.lock@gmail.com>
2025-07-03 14:03:28 +0300

    Add useTranslations to UserPageInfo and Users values to lang messages

--- src/modules/users/ui/components/UserPageInfo.tsx
+++ src/modules/users/ui/components/UserPageInfo.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useClerk, useAuth } from "@clerk/nextjs";
+import { useTranslations } from "next-intl";
 
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -7,9 +8,9 @@ import { UserAvatar } from "@/components/user-avatar";
 
 import { SubscriptionButton } from "@/modules/subscriptions/ui/components/SubscriptionButton";
 
+import { cn } from "@/lib/utils";
 import { UserGetOneOutput } from "../../types";
 import { useSubscription } from "@/modules/subscriptions/hooks/useSubscription";
-import { cn } from "@/lib/utils";
 
 interface UserPageInfoProps {
   user: UserGetOneOutput;
@@ -49,6 +50,7 @@ export const UserPageInfoSkeleton = () => {
 export const UserPageInfo = ({ user }: UserPageInfoProps) => {
   const clerk = useClerk();
   const { userId, isLoaded } = useAuth();
+  const t = useTranslations("Users");
 
   const { isPending, onClick } = useSubscription({
     userId: user.id,
@@ -75,9 +77,13 @@ export const UserPageInfo = ({ user }: UserPageInfoProps) => {
           <div className="flex-1 min-w-0">
             <h1 className="text-xl font-bold">{user.name}</h1>
             <div className="flex items-center gap-1 text-xs text-muted-foreground mt-1">
-              <span>{user.ssubscriberCount} subscribers</span>
+              <span>
+                {user.ssubscriberCount} {t("subscribers")}
+              </span>
               <span>&bull;</span>
-              <span>{user.videoCount} videos</span>
+              <span>
+                {user.videoCount} {t("videos")}
+              </span>
             </div>
           </div>
         </div>
@@ -88,7 +94,7 @@ export const UserPageInfo = ({ user }: UserPageInfoProps) => {
             variant="secondary"
             className="w-full mt-3 rounded-full"
           >
-            <Link href="/studio">Go to studio</Link>
+            <Link href="/studio">{t("btnGoToStudio")}</Link>
           </Button>
         ) : (
           <SubscriptionButton
@@ -120,14 +126,18 @@ export const UserPageInfo = ({ user }: UserPageInfoProps) => {
         <div className="flex-1 min-w-0">
           <h1 className="text-4xl font-bold">{user.name}</h1>
           <div className="flex items-center gap-1 text-sm text-muted-foreground mt-3">
-            <span>{user.ssubscriberCount} subscribers</span>
+            <span>
+              {user.ssubscriberCount} {t("subscribers")}
+            </span>
             <span>&bull;</span>
-            <span>{user.videoCount} videos</span>
+            <span>
+              {user.videoCount} {t("videos")}
+            </span>
           </div>
 
           {userId === user.clerkId ? (
             <Button asChild variant="secondary" className="mt-3 rounded-full">
-              <Link href="/studio">Go to studio</Link>
+              <Link href="/studio">{t("btnGoToStudio")}</Link>
             </Button>
           ) : (
             <SubscriptionButton
